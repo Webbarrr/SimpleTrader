da4034bae4dbe11bf5f1708914da40ee37ab03a9
James Webber <James.Webber@cansfordlabs.co.uk>
2020-07-08 22:45:10 +0100

    completed https://youtu.be/N09UM-RPGGM part 3 the main window loads blank every time

---
+++ SimpleTrader.WPF/States/Navigators/INavigator.cs
@@ -0,0 +1,19 @@
+﻿using SimpleTrader.WPF.ViewModels;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.States.Navigators
+{
+    public enum ViewType
+    {
+        Home,
+        Portfolio
+    }
+
+    public interface INavigator
+    {
+        ViewModelBase CurrentViewModel { get; set; }
+        ICommand UpdateCurrentViewModelCommand { get; }
+    }
+}
---
+++ SimpleTrader.WPF/States/Navigators/Navigator.cs
@@ -0,0 +1,27 @@
+﻿using SimpleTrader.WPF.Commands;
+using SimpleTrader.WPF.Models;
+using SimpleTrader.WPF.ViewModels;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.States.Navigators
+{
+    public class Navigator : ObservableObject, INavigator
+    {
+        private ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set 
+            { 
+                _currentViewModel = value;
+                this.OnPropertyChanged(nameof(this.CurrentViewModel));
+            }
+        }
+
+
+        public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+    }
+}
