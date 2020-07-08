a972f79b5c5576a4148011a694cec49926c5a843
James Webber <James.Webber@cansfordlabs.co.uk>
2020-07-08 15:26:54 +0100

    Add project files.

---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContext : DbContext
+    {
+        public DbSet<User> Users{ get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost;database=SimpleTraderDB;trusted_connection=true;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
